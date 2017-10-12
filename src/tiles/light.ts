af5802196db84faa2f60190851ea99cf02042008
Robey Pointer <robeypointer@gmail.com>
2017-10-12 12:05:03 -0700

    light will now turn off after 5 ticks.

--- src/tiles/light.ts
+++ src/tiles/light.ts
@@ -1,5 +1,8 @@
+import { range } from "../common/arrays";
 import { Action, ElectronAction, NEXT_CLOCKWISE, Orientation, Tile } from "../models";
 import { TileResources } from "./resources";
+
+const DEFAULT_POWER = 5;
 
 export class Light implements Tile {
   static resources = new TileResources();
@@ -8,13 +11,16 @@ export class Light implements Tile {
   element: HTMLElement = Light.resources.getClone(this.power, this);
   x = 0;
   y = 0;
+  activated = false;
 
   static async load(): Promise<void> {
     const cross = this.resources.byId("tile-wire-cross");
     const lightOn = this.resources.byId("tile-light-on");
     const lightOff = this.resources.byId("tile-light-off");
     this.resources.images[0] = await this.resources.stack([ cross, lightOff ]);
-    this.resources.images[1] = await this.resources.stack([ cross, lightOn ]);
+    await Promise.all(range(1, DEFAULT_POWER + 1).map(async i => {
+      this.resources.images[i] = await this.resources.stack([ cross, lightOn ]);
+    }));
   }
 
   rotate(variant?: number): Tile {
@@ -25,13 +31,27 @@ export class Light implements Tile {
     return this.power;
   }
 
-  // onTick(): TickAction {
-  //   if (this.triggered) return TickAction.stopPolling;
-  //   this.triggered = true;
-  //   return TickAction.spawn(this.orientation);
-  // }
+  onElectron(orientation: Orientation): ElectronAction {
+    this.activated = true;
+    this.power = DEFAULT_POWER;
+    return ElectronAction.die.withAction(this.morphImage());
+  }
+
+  onTick(): Action {
+    if (this.power > 0) this.power--;
+    if (this.power == 0) this.activated = false;
+    return this.morphImage();
+  }
 
   reset() {
+    this.activated = false;
     this.power = 0;
+    this.element = Light.resources.getClone(0, this);
+  }
+
+  morphImage(): Action {
+    const oldImage = this.element;
+    this.element = Light.resources.getClone(this.power, this);
+    return Action.changeImage(oldImage, this.element);
   }
 }
